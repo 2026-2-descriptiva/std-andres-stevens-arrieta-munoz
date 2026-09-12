40d9eebd17a8b430dfcbe6539c89760aed2ae25e
anarrietam <anarrietam@unal.edu.co>
2026-09-11 19:05:29 -0500

    Update Punto 1

--- PRE_02_mapreduce/src/word_count_1.py
+++ PRE_02_mapreduce/src/word_count_1.py
@@ -13,6 +13,7 @@ if os.path.exists(INPUT_FOLDER):
 else:
     os.makedirs(INPUT_FOLDER)
 
+
 n = 1000
 
 for file in glob.glob(f"{DATA_FOLDER}/*"):
@@ -33,3 +34,50 @@ for file in glob.glob(f"{DATA_FOLDER}/*"):
         with open(f"{INPUT_FOLDER}/{new_filename}", "w", encoding="utf-8") as f2:
             f2.write(text)
 
+
+start_time = time.time()
+
+sequence = []
+files = glob.glob(f"{INPUT_FOLDER}/*")
+for file in files:
+    with open(file, "r", encoding="utf-8") as f:
+        for line in f:
+            sequence.append((file, line))
+
+pairs_sequence = []
+for _, line in sequence:
+    line = line.lower()
+    line = line.translate(str.maketrans("", "", string.punctuation))
+    line = line.replace("\n", "")
+    words = line.split()
+    pairs_sequence.extend([(word, 1) for word in words])
+
+pairs_sequence = sorted(pairs_sequence)
+
+result = []
+for key, value in pairs_sequence:
+
+
+    if result and result[-1][0] == key:
+        result[-1] = (key, result[-1][1] + value)
+    else:
+        result.append((key, value))
+
+if os.path.exists(OUTPUT_FOLDER):
+    for file in glob.glob(f"{OUTPUT_FOLDER}/*"):
+        os.remove(file)
+else:
+    os.makedirs(OUTPUT_FOLDER)
+
+
+with open(f"{OUTPUT_FOLDER}/part-00000", "w", encoding="utf-8") as f:
+    for key, value in result:
+        f.write(f"{key}\t{value}\n")
+
+
+with open(f"{OUTPUT_FOLDER}/_SUCCESS", "w", encoding="utf-8") as f:
+    f.write("")
+
+
+end_time = time.time()
+print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
