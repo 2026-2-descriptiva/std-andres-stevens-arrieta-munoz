f84c9ef0484fd70731ee03df7266bc6f6eb0a338
anarrietam <anarrietam@unal.edu.co>
2026-09-25 15:05:08 -0500

    PRE_04 Solucion

--- PRE_04_limpieza/src/diagnostics.py
+++ PRE_04_limpieza/src/diagnostics.py
@@ -5,7 +5,11 @@ OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
 
 def main():
     df = pd.read_csv(OUTPUT_FILE)
-    series = df["purchase_date"]
+    series = df["weight"]
+
+    # Ayuda a ver si hay fechas duplicadas, pero no es necesario para el resultado final
+    # series = series.str.replace(r"\d", "d", regex=True)
+    # series = series.drop_duplicates()
 
 
     series = series.sort_values()
